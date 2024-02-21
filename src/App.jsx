8e18761381c985a2f3010c006e555383f8857219
Sergio Ruiz <sergioruizrock@gmail.com>
2024-02-21 20:04:30 -0300

    Se realiza maquetado prueba para verificar el funcionamiento de las librerias instaladas

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+import { Container } from 'react-bootstrap'
 import './App.css'
 
 function App() {
 
   return (
     <>
+    <header className="bg-dark text-light"> 
+      <h1 className='text-center'>Titulo</h1>
+    </header>
+    <Container className='mainContainer'></Container>
+    <footer className="bg-dark text-light">
+      <p className='text-center'>&copy; Todos los derechos reservados</p>
+    </footer>
     </>
   )
 }
