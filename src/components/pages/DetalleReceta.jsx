ce89cdeb3200cf831612d0ed0f91ecee49bc082b
Sergio Ruiz <sergioruizrock@gmail.com>
2024-02-26 08:04:10 -0300

    Se creo el componente DetalleReceta

---
+++ src/components/pages/DetalleReceta.jsx
@@ -0,0 +1,38 @@
+const DetalleReceta = () => {
+  return (
+    <section>
+      <div className="row d-flex justify-content-between py-3">
+        <div className="col-12 col-md-4">
+          <img src="" alt="" />
+        </div>
+        <div className="col-12 col-md-8">
+          <h3>Detalles del producto</h3>
+          <hr />
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. A eos quos
+            consectetur quibusdam repellendus atque, tempore deleniti
+            voluptatibus, distinctio maiores esse aperiam accusantium!
+            Perferendis ab est doloremque temporibus corrupti laboriosam quos
+            cupiditate dicta hic repellendus. Beatae sit molestias expedita
+            sapiente fugiat enim harum odit ullam? Vitae optio quas delectus
+            accusantium, sunt adipisci nemo sit, eveniet aut perspiciatis cum
+            obcaecati ea tenetur odio excepturi dicta recusandae neque ex
+            quidem. Eius, nam eum laboriosam officia, neque quam beatae facilis
+            nobis illo dolorem esse assumenda veniam sit omnis est magnam modi.
+            Optio ducimus quod ut saepe dolorem iste a pariatur qui possimus
+            ipsam quisquam minima porro facilis aperiam, temporibus dolorum
+            asperiores ea ad! Iure facere at magni esse animi similique
+            molestias placeat soluta error, voluptates aspernatur inventore
+            sequi voluptas exercitationem doloremque earum laboriosam, saepe
+            provident explicabo amet rerum. Ipsum, reprehenderit, doloribus
+            laborum quaerat quasi deleniti quas vel adipisci placeat, natus et
+            repudiandae repellendus?
+          </p>
+          <a className="btn btn-primary" href="">Volver</a>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default DetalleReceta;
