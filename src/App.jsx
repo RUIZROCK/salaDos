4cff808c432ab30c3c4d1a19359bc689dfbf4b4c
Sergio Ruiz <sergioruizrock@gmail.com>
2024-02-27 06:35:27 -0300

    se corrigio la aplicacion de router dom

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,23 @@ import './App.css'
 import { Route, Routes } from "react-router"
 import Menu from "./components/common/Menu"
 import Administracion from "./components/pages/Administracion"
+import { BrowserRouter } from "react-router-dom"
 
 function App() {
 
   return (
     <>
-    <Menu></Menu>
     
+    <BrowserRouter>
+    <Menu></Menu>
     <Routes>
       <Route exact path='/Administracion' element={<Administracion></Administracion>}></Route>
     </Routes>
-
-
     <footer className="bg-dark text-light">
       <p className='text-center'>&copy; Todos los derechos reservados</p>
     </footer>
+    
+    </BrowserRouter>
     </>
   )
 }
