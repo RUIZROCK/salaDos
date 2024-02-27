3ed6cd8353bf5b5ba15929968886f997857c6052
Sergio Ruiz <sergioruizrock@gmail.com>
2024-02-27 07:59:40 -0300

    se realizacion modificaciones en los componentes formulario y detalles

--- src/components/pages/FormularioReceta.jsx
+++ src/components/pages/FormularioReceta.jsx
@@ -12,8 +12,7 @@ const FormularioReceta = () => {
             <Form.Control
               required
               type="text"
-              placeholder="First name"
-              defaultValue="Mark"
+              placeholder="Ej: Asado"
             />
             <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
           </Form.Group>
@@ -36,8 +35,7 @@ const FormularioReceta = () => {
             <Form.Control
               required
               type="text"
-              placeholder="Last name"
-              defaultValue="Otto"
+              placeholder="Ej: Alto asaaaaao"
             />
             <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
           </Form.Group>
@@ -45,7 +43,7 @@ const FormularioReceta = () => {
 
           <Form.Group md="3" controlId="validationCustom04">
             <Form.Label>Detalles</Form.Label>
-            <Form.Control type="text" placeholder="State" required />
+            <Form.Control type="text" placeholder="Ej: Un asado se realiza..." required />
             <Form.Control.Feedback type="invalid">
               Please provide a valid state.
             </Form.Control.Feedback>
@@ -54,24 +52,13 @@ const FormularioReceta = () => {
 
           <Form.Group md="3" controlId="validationCustom05">
             <Form.Label>URL imagen</Form.Label>
-            <Form.Control type="text" placeholder="Zip" required />
+            <Form.Control type="text" placeholder="Ej: img.com.ar/img12313,jpg" required />
             <Form.Control.Feedback type="invalid">
               Please provide a valid zip.
             </Form.Control.Feedback>
           </Form.Group>
 
-
-        <Form.Group className="mb-3">
-          <Form.Check
-            required
-            label="Agree to terms and conditions"
-            feedback="You must agree before submitting."
-            feedbackType="invalid"
-          />
-        </Form.Group>
-
-
-        <Button type="submit">Submit form</Button>
+        <Button type="submit" className="my-3">Guardar</Button>
       </Form>
     </section>
     </Container>
