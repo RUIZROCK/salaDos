48e24b1f02eae810761f57a7e3a3b0f9f8e64b24
Sergio Ruiz <sergioruizrock@gmail.com>
2024-02-27 06:04:59 -0300

    Se agrego el componente menu a App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 import { Container } from 'react-bootstrap'
 import './App.css'
+import { Menu } from "react-bootstrap/lib/Dropdown"
 
 function App() {
 
   return (
     <>
-    <header className="bg-dark text-light"> 
-      <h1 className='text-center'>Titulo</h1>
-    </header>
+    <Menu></Menu>
     <Container className='mainContainer'></Container>
     <footer className="bg-dark text-light">
       <p className='text-center'>&copy; Todos los derechos reservados</p>
