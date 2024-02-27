c13895510372148a6595617a0c63f1801bed8072
Sergio Ruiz <sergioruizrock@gmail.com>
2024-02-27 06:21:04 -0300

    correccion y aplicacion de router dom en app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import "bootstrap/dist/css/bootstrap.min.css"
-import { Container } from 'react-bootstrap'
 import './App.css'
-import { Menu } from "react-bootstrap/lib/Dropdown"
+import { Route, Routes } from "react-router"
+import Menu from "./components/common/Menu"
+import Administracion from "./components/pages/Administracion"
 
 function App() {
 
   return (
     <>
     <Menu></Menu>
-    <Container className='mainContainer'></Container>
+    
+    <Routes>
+      <Route exact path='/Administracion' element={<Administracion></Administracion>}></Route>
+    </Routes>
+
+
     <footer className="bg-dark text-light">
       <p className='text-center'>&copy; Todos los derechos reservados</p>
     </footer>
