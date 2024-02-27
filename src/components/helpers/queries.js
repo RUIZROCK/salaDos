c0c2f2753cf100c1e7d2a541416b6460751fd058
Sergio Ruiz <sergioruizrock@gmail.com>
2024-02-27 15:48:12 -0300

    se agrega la funcion leerRecetasAPI

--- src/components/helpers/queries.js
+++ src/components/helpers/queries.js
@@ -1,16 +1,28 @@
 const URI_Receta = import.meta.env.VITE_API_RECETA;
 
-export const nuevoRecetaAPI = async (producto)=>{
+export const nuevoRecetaAPI = async (receta)=>{
     try{
         const respuesta= await fetch(URI_Receta, {
             method:"POST",
             headers:{
                 "Content-Type":"application/json"
             },
-            body:JSON.stringify(producto)
+            body:JSON.stringify(receta)
         });
         return respuesta;
     }catch(error){
         console.log(error);
     }
 }
+
+export const leerProductosAPI = async () => {
+    try {
+      const respuesta = await fetch(URI_Receta);
+      console.log(respuesta);
+      return respuesta;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  
