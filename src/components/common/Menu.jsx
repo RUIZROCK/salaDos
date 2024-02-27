3a70bb7dd3e49d8a81ea9b8ed7b621c34f998d7b
Sergio Ruiz <sergioruizrock@gmail.com>
2024-02-27 06:36:40 -0300

    se aplicacion correcciones en el navbar en relacion de router dom y css

--- src/components/common/Menu.jsx
+++ src/components/common/Menu.jsx
@@ -10,9 +10,9 @@ const Menu = () => {
           <Navbar.Toggle aria-controls="basic-navbar-nav" className="bg-light" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto d-flex justify-content-end w-100">
-              <NavLink end className="text-light" href="#home">Inicio</NavLink>
-              <NavLink end className="text-light" to="/Administracion">Administracion</NavLink>
-              <NavLink end className="text-light" href="#link">Registrate!</NavLink>
+              <NavLink end className="text-light nav-link" href="#home">Inicio</NavLink>
+              <NavLink end className="text-light nav-link" to="/Administracion">Administracion</NavLink>
+              <NavLink end className="text-light nav-link" href="#link">Registrate!</NavLink>
             </Nav>
           </Navbar.Collapse>
         </Container>
