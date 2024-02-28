9f9f29676c46806656955e3e34b5b3d8a92e6871
shiruiz2208 <gise.13.ruiz@gmail.com>
2024-02-28 19:22:51 -0300

    agregamos grilla de card en inicio

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,12 +1,12 @@
 import {Container,Row,Col}  from 'react-bootstrap'
-import logo from '../../assets/logo.png'
+import logo1 from '../../assets/logo1.png'
 
 const Footer = () => {
     return (
         <footer className="text-light p-2">
             <Container>
                 <Row className='align-items-center'>
-                    <Col xs={2}><img className='logo' src={logo} alt="Logo" /></Col>
+                    <Col xs={2}><img className='logo' src={logo1} alt="Logo" /></Col>
                     <Col xs={{ offset: 1, span: 5 }} className='align-items-center'>
                          <p> <i className="bi bi-geo-alt"></i> Dirección : Gral Paz 545 - San Miguel de Tucumán </p>
                     </Col>
--- src/components/common/Menu.jsx
+++ src/components/common/Menu.jsx
@@ -1,13 +1,13 @@
 import { Container, Nav, Navbar } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
-import logo from '../../assets/logo.png'
+import logo1 from '../../assets/logo1.png'
 
 const Menu = () => {
   return (
     <header>
       <Navbar expand="lg" className="menu text-light p-2">
         <Container>
-          <Navbar.Brand href="#home"><img className='logo' src={logo} alt="Logo" /></Navbar.Brand>
+          <Navbar.Brand href="#home"><img className='logo' src={logo1} alt="Logo" /></Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" className="bg-light" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto d-flex justify-content-end w-100">
